58e9dc951626a386f6348ccf2f00004abc9e3303
ViktorKernytskyi <vihtor@ukr.net>
2024-10-12 23:52:43 +0300

    page allows you to view and add users

--- public/select.php
+++ public/select.php
@@ -1,23 +1,68 @@
 <?php
-include('Database.php');
+
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Form</title>
+    <title>Вибір користувача</title>
 </head>
 <body>
-    <h1>Додати користувача</h1>
-    <form action="process.php" method="POST">
-        <label for="name">Ім'я:</label>
-        <input type="text" name="name" id="name" required><br><br>
+<h1>Вибір користувача за іменем</h1>
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required><br><br>
-        <input type="submit" value="Додати">
-    </form>
+<form action="select.php" method="POST">
+    <label for="name">Ім'я:</label>
+    <input type="text" name="name" required><br><br>
+    <input type="submit" value="Знайти">
+</form>
+
+<!-- Виведення результатів -->
+<?php
+    if (isset($users)) {
+        echo "<h2>Результати:</h2>";
+foreach ($users as $user) {
+echo "<p>{$user['first_name']} {$user['last_name']}</p>";
+}
+}
+?>
 </body>
-</html>git add
+</html>
+
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Список Користувачів</title>
+</head>
+<body>
+<h1>Список Користувачів</h1>
+<a href="add.php">Додати Користувача</a> <!-- Посилання на сторінку додавання -->
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Ім'я</th>
+        <th>Прізвище</th>
+        <th>Email</th>
+        <th>Телефон</th>
+        <th>Дії</th>
+    </tr>
+<!--    --><?php //foreach ($users as $user): ?>
+        <tr>
+<!--            <td>--><?php //echo $user['id']; ?><!--</td>-->
+<!--            <td>--><?php //echo $user['first_name']; ?><!--</td>-->
+<!--            <td>--><?php //echo $user['last_name']; ?><!--</td>-->
+<!--            <td>--><?php //echo $user['email']; ?><!--</td>-->
+<!--            <td>--><?php //echo $user['phone']; ?><!--</td>-->
+            <td>
+<!--                <a href="edit.php?id=--><?php //echo $user['id']; ?><!--">Редагувати</a> |-->
+<!--                <a href="delete.php?id=--><?php //echo $user['id']; ?><!--">Видалити</a>-->
+            </td>
+        </tr>
+<!--    --><?php //endforeach; ?>
+</table>
+</body>
+</html>
